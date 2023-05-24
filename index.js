72765f7a75539d1bdf904c9e11328210751a73c3
dincerdegre <dincerdegre@gmail.com>
2023-05-24 04:57:52 +0300

    added docker and docker-compose support for node compability

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const errorMiddleware = require("./src/middlewares/errorMiddleware");
 const apiRouter = require("./src/routers/apiRouter");
 const rateLimit = require("express-rate-limit");
 const app = express();
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerDoc = require("./src/data/swagger.json");
+const port = 3002;
 
-app.set('trust proxy', 1);
+// app.set("trust proxy", 0);
 const limiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000,
   max: 100,
@@ -26,7 +29,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept",
   );
   next();
 });
@@ -35,21 +38,21 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   console.log(res.getHeader("ratelimit-remaining"));
-  res
-    .status(200)
-    .json({
-      message: "Welcome to TCMB JSON API.",
-      ratelimit:
-        "You have " +
-        res.getHeader("ratelimit-remaining") +
-        " request Remaining for 24 Hours",
-    });
+  res.status(200).json({
+    message: "qqasaqaaWelcome to TCMB JSON API.",
+    ratelimit:
+      "You have " +
+      res.getHeader("ratelimit-remaining") +
+      " request Remaining for 24 Hours",
+  });
 });
+
+// app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use("/api", apiRouter);
 
 app.use(errorMiddleware);
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   // PORT = 3000
-  console.log("listening on port " + process.env.PORT);
+  console.log("listening on port " + port);
 });
