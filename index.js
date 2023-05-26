a67cab597037f29a59e62fab86b4401aaf994ada
dincerdegre <dincerdegre@gmail.com>
2023-05-26 03:30:36 +0300

    added ratelimit number with dotenv

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ const app = express();
 const swaggerUi = require("swagger-ui-express");
 const openApiDoc = require("./src/data/openapi.json");
 const port = process.env.PORT;
+const maxRateLimit = process.env.RATELIMIT_MAX;
 
 app.set("trust proxy", 1);
 const limiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000,
-  max: 100,
+  max: maxRateLimit,
   message: {
     status: 429,
-    error: "You have exceeded the 100 requests in 24 hrs limit!",
+    error: "You have exceeded Maximum requests in 24 hrs limit!",
   },
   standardHeaders: true,
   legacyHeaders: false,
@@ -39,11 +40,18 @@ app.use(cors());
 app.get("/", (req, res) => {
   console.log(res.getHeader("ratelimit-remaining"));
   res.status(200).json({
-    message: "Welcome to TCMB JSON API.",
+    author: "Dinçer Değre",
+    github: "https://github.com/dincerdegre",
+    baseCurrency: "TRY",
+    documentation: {
+      "/docs": "For API Documentation",
+      "/api": "For API Routes",
+    },
     ratelimit:
       "You have " +
       res.getHeader("ratelimit-remaining") +
       " request Remaining for 24 Hours",
+    message: "Welcome to TCMB JSON API.",
   });
 });
 
